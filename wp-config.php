7298e33021950f1708a4f60e3e6dab0308a5f82f
vsrzone <vikumsri@gmail.com>
2015-05-19 14:39:38 +0530

    config changed, styles for single page content added

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'getnailed_usr');
 define('DB_PASSWORD', 'eluwa123');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.ingenslk.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
