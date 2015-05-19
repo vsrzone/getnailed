13adb0adb8ef7a0291256760f7c7ee5d8536e259
vsrzone <vikumsri@gmail.com>
2015-05-19 11:26:13 +0530

    host changed

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'getnailed_usr');
 define('DB_PASSWORD', 'eluwa123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.ingenslk.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
